f1647a3a3d640b2643d6315cc2563eb525f666c0
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-11-25 19:12:21 +0530

    Duplicate Element

---
+++ Arrays/duplicateElement.java
@@ -0,0 +1,15 @@
+public class duplicateElement {
+    public int removeDuplicates(int[] nums) {
+       if(nums.length==0){
+        return 0;
+       }
+       int x=0;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i]!=nums[x]){
+            x++;
+            nums[x]=nums[i];
+        }
+       }
+       return x+1;
+    }
+}
