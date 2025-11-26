36a50bb1734d2d35f30fdbb2c610c5d2b96f8f07
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-11-26 18:57:09 +0530

    H-INdex

---
+++ Arrays/HIndex.java
@@ -0,0 +1,19 @@
+public class HIndex {
+     public int hIndex(int[] citations) {
+        int n=citations.length;
+        int []arr=new int [n+1];
+        for(int c:citations){
+            if(c>n)
+            arr[n]++;
+            else
+            arr[c]++;
+        }
+        int count=0;
+        for(int i=n;i>=0;i--){
+            count =count+arr[i];
+            if(count>=i)
+            return i;
+        }
+        return 0;
+    }
+}
