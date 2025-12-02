5a8a6f92409f0d0de60bc86ab62bce5592f0e520
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-12-02 20:23:32 +0530

    two sum medium

---
+++ Arrays/twoSum.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+public class twoSum {
+    
+    static public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                System.out.println("nums[i]=" + numbers[i]);
+                System.out.println("nums[j]=" + numbers[j]);
+                System.out.println("addition above element is = " + (numbers[i] + numbers[j]));
+                if ((numbers[i] + numbers[j]) == target) {
+                    return new int[] { i + 1, j + 1 };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+    //better
+     static public int[] twoSumbetter(int[] numbers, int target) {
+        int n=numbers.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int a=numbers[i];
+            int more=target-a;
+            if(map.containsKey(more)){
+                return new int[]{map.get(more),i};
+            }
+            map.put(a, i);
+        }
+        return new int[] {-1,-1};
+     }
+    //  optimal
+    static public int[] twoSumbetterop(int[] numbers, int target) {
+        int n=numbers.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+                return new int[]{left+1,right+1};
+            }
+            if(sum>target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+}
--- BitManipulation/AddBinary.java
+++ BitManipulation/AddBinary.java
@@ -1,5 +1,22 @@
-package BitManipulation;
+
 
 public class AddBinary {
-    
+     public String addBinary(String a, String b) {
+  int i = a.length() - 1;
+    int j = b.length() - 1;
+    int carry = 0;
+    StringBuilder ans = new StringBuilder();
+
+    while (i >= 0 || j >= 0 || carry == 1) {
+        int sum = carry;
+
+        if (i >= 0) sum += a.charAt(i--) - '0';
+        if (j >= 0) sum += b.charAt(j--) - '0';
+
+        ans.append(sum % 2);   // write the current bit
+        carry = sum / 2;       // update carry
+    }
+
+    return ans.reverse().toString();
+    }
 }
