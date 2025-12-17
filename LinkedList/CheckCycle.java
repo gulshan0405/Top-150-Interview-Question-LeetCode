98ba9eea8abeff402a77a89f005eb95ec36049cc
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-12-17 22:11:12 +0530

    Linked List Basic and Question

---
+++ LinkedList/CheckCycle.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class CheckCycle {
+    public static void main(String[] args) {
+        LinkedList<Integer> list=new LinkedList<>();
+        list.add(1);
+        list.add(5);
+        System.out.println(list);
+    }
+}
