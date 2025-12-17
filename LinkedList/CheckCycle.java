0bc2673eca8a8f481465ad4604828a08fe81da05
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-12-17 22:32:26 +0530

    abC

--- LinkedList/CheckCycle.java
+++ LinkedList/CheckCycle.java
@@ -1,10 +1,58 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class CheckCycle {
-    public static void main(String[] args) {
-        LinkedList<Integer> list=new LinkedList<>();
-        list.add(1);
-        list.add(5);
-        System.out.println(list);
+   
+   static public class LL{
+    private ListNode head;
+    private int size;
+
+    public LL(){
+        size=0;
     }
+
+      public boolean hasCycle(ListNode head) {
+
+        //optimal approach
+        // ListNode fast=head;
+        // ListNode slow=head;
+
+        // while(fast!=null && fast.next!=null){
+        //     fast=fast.next.next;
+        //     slow=slow.next;
+        //     if(fast==slow){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //non-optimal approach
+        HashSet<ListNode> map=new HashSet<>();
+        ListNode node=head;
+        while(node!=null){
+            if(!map.contains(node)){
+               map.add(node);
+               node=node.next;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class ListNode{
+        private int value;
+        private ListNode next;
+
+        public ListNode(int value){
+            this.value=value;
+        }
+        public ListNode(int value,ListNode next){
+            this.value=value;
+            this.next=next;
+        }
+    }
+   }
+
 }
