7558c5f3e9e4aa715ed866d427d53fe63f7f241d
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-12-19 19:09:37 +0530

    Add two numbers

---
+++ LinkedList/AddTwoNumber.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+
+public class AddTwoNumber {
+     static public class LL{
+    private ListNode head;
+    private int size;
+
+    public LL(){
+        size=0;
+    }
+ public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0); // placeholder for the result
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            
+            if (l1 != null) {
+                sum += l1.value;
+                l1 = l1.next;
+            }
+            
+            if (l2 != null) {
+                sum += l2.value;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        
+        return dummy.next;
+    }
+    private class ListNode{
+        private int value;
+        private ListNode next;
+
+        public ListNode(int value){
+            this.value=value;
+        }
+        public ListNode(int value,ListNode next){
+            this.value=value;
+            this.next=next;
+        }
+    }
+   }
+
+
+    
+}
--- Maths/SquareRoot.java
+++ Maths/SquareRoot.java
@@ -1,14 +1,35 @@
 
 
 public class SquareRoot {
-    public static void main(String[] args) {
-        int n=15;
-        int key=111;
-        for(int i=1;i<n;i++){
-            if(i*i>key){
-                System.out.println(i-1);
+    public int mySqrt(int x) {
+        //optimal approach
+        //     if (x == 0 || x == 1) return x;
+
+        // int left = 1, right = x / 2, ans = 0;
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
+        //     long sq = (long) mid * mid; // use long to avoid overflow
+
+        //     if (sq == x) {
+        //         return mid;
+        //     } else if (sq < x) {
+        //         ans = mid; // possible answer
+        //         left = mid + 1;
+        //     } else {
+        //         right = mid - 1;
+        //     }
+        // }
+        // return ans;
+
+        //brute force
+        long n = Integer.MAX_VALUE;
+        long ans=0;
+        for (long i = 1; i < n; i++) {
+            if (i * i > x) {
+                ans=i-1;
                 break;
             }
         }
+        return (int)ans;
     }
 }
